3dd9ab3a94273b19e5a6554a48d090c2688c3b91
Roogland <akhimierowland@gmail.com>
2023-05-16 17:29:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Fragment } from "react";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+const containerStyle = {
+  width: '94%',
+  height: '720px',
+  margin: 'auto',
+  position: 'relative'
+};
+
+const center = {
+  lat: 6.524379,
+  lng: 3.379206
+};
+const App = ()=>{
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyCBo52FdbKLljKJJpRZe5AmOy5FbWgyL6I"  
+  })
+
+const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  
+  return <div style={{position:'relative'}}>
+    <div style={{position:'absolute',top:10,left:10,backgroundColor:'royalBlue',padding:15,zIndex:999}}>
+      <h1>Google Map In react App.</h1>
     </div>
-  );
+    {isLoaded ? <GoogleMap
+    mapContainerStyle={containerStyle}
+    center={center}
+    zoom={8}
+    onLoad={onLoad}
+    onUnmount={onUnmount}
+  >
+    { /* Child components, such as markers, info windows, etc. */ }
+    <></>
+  </GoogleMap>
+  :<></>
+    }
+
+  </div>
+
+  
 }
 
 export default App;
